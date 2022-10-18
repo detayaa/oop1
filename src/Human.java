ee00a1a7e0e659d6503625b5bbf23dc171a10c5c
detayaa <drugochenko.taya@mail.ru>
2022-10-18 20:50:44 +0700

    2

--- src/Human.java
+++ src/Human.java
@@ -2,15 +2,17 @@ public class Human {
     int dateOfBirth;
     String name;
     String city;
+    String post;
 
-    Human(String name, String city, int dateOfBirth) {
+    Human(String name, String city, int dateOfBirth, String post) {
         this.name = name;
         this.city = city;
         this.dateOfBirth = dateOfBirth;
+        this.post = post;
     }
 
     void human() {
-        System.out.println("Привет! Меня зовут " + name + "! Я из города " + city + " и я родился в " + dateOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + "! Я из города " + city + ", я родился в " + dateOfBirth + " году. И я работаю на должности " + post + ". Будем знакомы!");
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human("Максим", "Минск", 1987);
+        Human maksim = new Human("Максим", "Минск", 1987, "бренд-менеджер");
         maksim.human();
-        Human anya = new Human("Аня", "Москва", 1993);
+        Human anya = new Human("Аня", "Москва", 1993,"методист образовательных программ");
         anya.human();
-        Human katya = new Human("Катя", "Калиниград", 1992);
+        Human katya = new Human("Катя", "Калиниград", 1992, "продакт-менеджер");
         katya.human();
-        Human artem = new Human("Артем", "Москва", 1991);
+        Human artem = new Human("Артем", "Москва", 1991, "директор по развитию бизнеса");
         artem.human();
 
     }
